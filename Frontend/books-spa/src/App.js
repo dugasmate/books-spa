6835e008845d60b5ede56f00071f204b7db477c7
dugasmate <dugasmate@gmail.com>
2018-12-03 15:50:49 +0100

    fix styling

--- Frontend/books-spa/src/App.js
+++ Frontend/books-spa/src/App.js
@@ -11,40 +11,38 @@ class App extends Component {
       table: [],
       title: undefined,
       author: undefined,
-      }
     }
+  }
 
-    handleChange(event) {
-      console.log(event.target.name, event.target.value);
-      this.setState({ [event.target.name]: event.target.value });
+  handleChange(event) {
+    this.setState({ [event.target.name]: event.target.value });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    const book = {
+      title: this.state.title,
+      author: this.state.author
     }
+    fetch("https://localhost:44307/index/", {
+      method: "POST",
+      body: JSON.stringify(book),
+      mode: "cors",
+      headers: new Headers({
+        "Content-Type": "application/json"
+      })
+    });
+    setTimeout(this.componentWillMount, 500)
+  };
 
-    handleSubmit(event){ 
-      event.preventDefault();
-      const book = { 
-        title: this.state.title,
-        author: this.state.author
-      }
-      fetch("https://localhost:44307/index/", {
-        method: "POST",
-        body: JSON.stringify(book),
-        mode: "cors",
-        headers: new Headers({
-          "Content-Type": "application/json"
-        })
-      });
-      setTimeout(this.componentWillMount, 500)
-     };
+  removeBook(id) {
+    axios.get(
+      "https://localhost:44307/index/" + id
+    );
+    setTimeout(this.componentWillMount, 500);
+  }
 
-     removeBook(id) {
-       console.log(id)
-      axios.get(
-        "https://localhost:44307/index/" + id
-      );
-      setTimeout(this.componentWillMount, 500);
-    }
-
-    componentWillMount = () => 
+  componentWillMount = () =>
     fetch("https://localhost:44307/index/", {
       mode: "cors"
     })
@@ -52,40 +50,33 @@ class App extends Component {
       .then(table => this.setState({ table }));
 
   render() {
-   const renderedTable = (
-    <form onSubmit={this.handleSubmit}>
-    <table className="Table">
-    <tr>
-      <th>
-        Author
-      </th>
-      <th>
-        Title
-      </th>
-      <th>
-      </th>
-    </tr>
-    {this.state.table.map(element => (
-     <tr>
-     <td>
-      {element.author}
-     </td>
-     <td>
-      {element.title}
-     </td>
-     <td>
-     <button type="button" className="X" onClick={() => this.removeBook(element.id)}> X </button>
-     </td>
-   </tr>
-    ))}
-  <tr>
-  <td><input type="text" placeholder="Author" onChange={this.handleChange} name="author"></input></td>
-  <td><input type="text" placeholder="Title" onChange={this.handleChange} name="title"></input></td>
-  <td><input type="submit" value="Add" /></td>
-    </tr>
-  </table>
-  </form>
-   )
+    const renderedTable = (
+      <form onSubmit={this.handleSubmit}>
+        <table className="Table">
+        <thead>
+          <tr>
+            <th>Author</th>
+            <th>Title</th>
+            <th></th>
+          </tr>
+          </thead>
+          <tbody>
+          {this.state.table.map(element => (
+            <tr>
+              <td>{element.author}</td>
+              <td>{element.title}</td>
+              <td><button type="button" className="X" onClick={() => this.removeBook(element.id)}> X </button></td>
+            </tr>
+          ))}
+          <tr>
+            <td><input type="text" placeholder="Author" onChange={this.handleChange} name="author"></input></td>
+            <td><input type="text" placeholder="Title" onChange={this.handleChange} name="title"></input></td>
+            <td><input type="submit" value="Add" /></td>
+          </tr>
+          </tbody>
+        </table>
+      </form>
+    )
 
     return (
       <div>{renderedTable}</div>
